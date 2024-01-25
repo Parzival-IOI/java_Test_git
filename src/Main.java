16c344d01931424d9c895659a07f56459b2135c8
Kimhouy Hor <kimhouy.hor@allweb.com.kh>
2024-01-25 20:01:48 +0700

    hello

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,33 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        Scanner scan = new Scanner(System.in);
     }
+}
+
+class Person {
+    private String name;
+    private String address;
+    Person() {}
+    Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+}
+
+class Student extends Person {
+    private String course;
+    private int number_course;
+    private String grade;
+
+    Student(String name, String address, int number_course) {
+        super(name, address);
+        this.number_course = number_course;
+    }
+
+    public void addCoureGrade(String course, String grade) {
+
+    }
+
 }
