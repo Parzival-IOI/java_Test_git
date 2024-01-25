2494115ae41023afa88996ba1e863ecccf02e9ab
Kimhouy Hor <kimhouy.hor@allweb.com.kh>
2024-01-25 20:20:47 +0700

    testing

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        Student stu;
+
+        stu = new Student("Parzival", "IOI", 3);
+
+        try {
+            stu.addCourseGrade("Spring Boot", "A");
+            stu.addCourseGrade("Laravel", "A");
+            stu.addCourseGrade("Next JS", "A");
+            stu.addCourseGrade(".Net", "A");
+        }
+        catch (Exception ex) {
+            System.out.println("Error : " + ex.getMessage());
+        }
+
+        stu.printGrade();
+
+        scan.close();
     }
 }
 
@@ -41,7 +59,7 @@ class Student extends Person {
 
     public void printGrade() {
         System.out.println("Name : " + name);
-        System.out.println("Address : "+ addresss);
+        System.out.println("Address : "+ address);
         System.out.println("Course Number : " + number_course);
         for (int i = 0; i < number_course; i++) {
             System.out.println("Course " + (i+1) + " : " + course[i]);
