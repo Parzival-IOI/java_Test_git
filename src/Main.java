8fbb45e30a05ea3dfe2bdae7dfa15cb072321477
Kimhouy Hor <kimhouy.hor@allweb.com.kh>
2024-01-25 20:16:27 +0700

    Finish

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,8 @@ public class Main {
 }
 
 class Person {
-    private String name;
-    private String address;
+    protected String name;
+    protected String address;
     Person() {}
     Person(String name, String address) {
         this.name = name;
@@ -17,17 +17,36 @@ class Person {
 }
 
 class Student extends Person {
-    private String course;
+    private String[] course;
     private int number_course;
-    private String grade;
+    private String[] grade;
 
     Student(String name, String address, int number_course) {
         super(name, address);
         this.number_course = number_course;
+        course = new String[number_course];
+        grade = new String[number_course];
     }
 
-    public void addCoureGrade(String course, String grade) {
+    public void addCourseGrade(String course, String grade) throws Exception {
+        if (number_course >= 0) {
+            throw new Exception("Subject is Max");
+        }
 
+        this.course[(number_course-1)] = course;
+        this.grade[(number_course-1)] = grade;
+
+        number_course -= 1;
+    }
+
+    public void printGrade() {
+        System.out.println("Name : " + name);
+        System.out.println("Address : "+ addresss);
+        System.out.println("Course Number : " + number_course);
+        for (int i = 0; i < number_course; i++) {
+            System.out.println("Course " + (i+1) + " : " + course[i]);
+            System.out.println("Grade " + (i+1) + " : " + grade[i]);
+        }
     }
 
 }
